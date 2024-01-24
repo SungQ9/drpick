0603a9b7fe7fd7f693457881e9284f11933cdfc6
jhalim9393 <jhalim9393@naver.com>
2024-01-24 10:00:11 +0900

    DOOSAN merge

--- src/Components/User/MyPage/Payment.jsx
+++ src/Components/User/MyPage/Payment.jsx
@@ -1,31 +1,31 @@
 // 결제수단등록
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import back from '../../../img/back-arrow-icon.png';
-import card from '../../../img/card-icon.png';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import back from "../../../img/back-arrow-icon.png";
+import card from "../../../img/card-icon.png";
 
 const Payment = () => {
   const navigate = useNavigate();
   return (
-    <div className='shortForm'>
-      <div id='paymentTitle' style={{ justifyContent: 'unset ' }}>
+    <div className="shortForm">
+      <div id="paymentTitle" style={{ justifyContent: "unset " }}>
         <img
-          className='backIcon'
+          className="backIcon"
           src={back}
           onClick={() => {
             navigate(-1);
           }}
-          alt='back'
+          alt="back"
         />
         <h2>결제수단등록</h2>
       </div>
-      <div className='paymentBody'>
+      <div className="paymentBody">
         <h3>신용/체크카드</h3>
-        <img src={card} alt='card' />
+        <img src={card} alt="card" />
         <h3>카드를 등록해주세요</h3>
-        <table id='priceBtnTable'>
+        <table id="priceBtnTable">
           <tr>
-            <td style={{ height: '100px' }}>카드추가</td>
+            <td style={{ height: "100px" }}>카드추가</td>
           </tr>
         </table>
       </div>
