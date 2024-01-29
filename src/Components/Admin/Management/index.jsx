f6b4679c19351e8a4c2e0d09e48fd4e3d963a004
jhalim9393 <jhalim9393@naver.com>
2024-01-29 13:54:33 +0900

    관리자 회원 목록 - 서브디테일주소

--- src/Components/Admin/Management/index.jsx
+++ src/Components/Admin/Management/index.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { useTokenContext } from '../../Context/TokenContext';
-import axios from 'axios';
-import headers from '../../SampleData/Headers';
-import ListTitle from '../../Layout/List/ListTitle';
-import List from '../../Layout/List';
-import Loading from '../../User/ImageSearch/Loading';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useTokenContext } from "../../Context/TokenContext";
+import axios from "axios";
+import headers from "../../SampleData/Headers";
+import ListTitle from "../../Layout/List/ListTitle";
+import List from "../../Layout/List";
+import Loading from "../../User/ImageSearch/Loading";
+import { response } from "express";
 
 const InquiryManage = () => {
   const location = useLocation();
   const { token, userAuth } = useTokenContext();
-  const selectedType = location.state?.selectedType || 'default';
-  const [title, setTitle] = useState('');
+  const selectedType = location.state?.selectedType || "default";
+  const [title, setTitle] = useState("");
   const [items, setItems] = useState();
   const [currentHeaders, setCurrentHeaders] = useState();
   const [isLoading, setIsLoading] = useState(true);
@@ -21,7 +22,7 @@ const InquiryManage = () => {
       Authorization: `Bearer ${token}`,
     },
     params: {
-      memberId: localStorage.getItem('userId'),
+      memberId: localStorage.getItem("userId"),
     },
   };
 
@@ -39,7 +40,7 @@ const InquiryManage = () => {
       setCurrentHeaders(headers);
       setTitle(titleKey);
     } catch (err) {
-      console.error('데이터 요청 에러:', err);
+      console.error("데이터 요청 에러:", err);
       // 에러 발생 시 대체 데이터 설정 가능
     } finally {
       setIsLoading(false);
@@ -48,75 +49,75 @@ const InquiryManage = () => {
 
   const convertInquiryType = (inquiryType) => {
     switch (inquiryType) {
-      case 'Q':
-        return '이용문의';
-      case 'C':
-        return '진료문의';
-      case 'P':
-        return '결제문의';
-      case 'D':
-        return '배송문의';
+      case "Q":
+        return "이용문의";
+      case "C":
+        return "진료문의";
+      case "P":
+        return "결제문의";
+      case "D":
+        return "배송문의";
       default:
-        return '기타';
+        return "기타";
     }
   };
 
   useEffect(() => {
     switch (selectedType) {
-      case 'user':
+      case "user":
         fetchInquiryData(
-          'http://localhost:8080/admin/getMemberList',
+          "http://localhost:8080/admin/getMemberList",
           headers.members,
-          '회원관리',
+          "회원관리"
         );
         break;
-      case 'doctor':
+      case "doctor":
         fetchInquiryData(
-          'http://localhost:8080/admin/getDoctorsList',
+          "http://localhost:8080/admin/getDoctorsList",
           headers.doctors,
-          '의사관리',
+          "의사관리"
         );
         break;
-      case 'request':
+      case "request":
         fetchInquiryData(
-          'http://localhost:8080/admin/getRegistRequestList',
+          "http://localhost:8080/admin/getRegistRequestList",
           headers.requestDoctors,
-          '등록요청목록',
+          "등록요청목록"
         );
         break;
-      case 'hospital':
+      case "hospital":
         fetchInquiryData(
-          'http://localhost:8080/admin/getHospitalList',
+          "http://localhost:8080/admin/getHospitalList",
           headers.hospitals,
-          '병원관리',
+          "병원관리"
         );
         break;
-      case 'drugstore':
+      case "drugstore":
         fetchInquiryData(
-          'http://localhost:8080/admin/getDrugstoreList',
+          "http://localhost:8080/admin/getDrugstoreList",
           headers.drugstores,
-          '약국관리',
+          "약국관리"
         );
         break;
-      case 'userInquiry':
+      case "userInquiry":
         fetchInquiryData(
-          'http://localhost:8080/admin/getMemberInquiryList',
+          "http://localhost:8080/admin/getMemberInquiryList",
           headers.inquiry,
-          '회원 문의',
+          "회원 문의"
         );
         break;
-      case 'doctorInquiry':
+      case "doctorInquiry":
         fetchInquiryData(
-          'http://localhost:8080/admin/getDoctorInquiryList',
+          "http://localhost:8080/admin/getDoctorInquiryList",
           headers.inquiry,
-          '의사 문의',
+          "의사 문의"
         );
         break;
-      case 'drugstoreInquiry':
+      case "drugstoreInquiry":
         fetchInquiryData(
-          'http://localhost:8080/admin/getDrugstoreInquiryList',
+          "http://localhost:8080/admin/getDrugstoreInquiryList",
           headers.inquiry,
-          '약국 문의',
+          "약국 문의"
         );
         break;
       default:
@@ -130,45 +131,45 @@ const InquiryManage = () => {
   }
 
   return (
-    <div className='listWrapper'>
+    <div className="listWrapper">
       <ListTitle title={title} />
 
       {/*회원관리,의사관리 */}
-      {(selectedType === 'user' || selectedType === 'doctor') && (
+      {(selectedType === "user" || selectedType === "doctor") && (
         <List
           headers={currentHeaders}
           items={items}
-          listbutton={'수정'}
-          buttonType={'N'}
+          listbutton={"수정"}
+          buttonType={"N"}
         />
       )}
 
       {/*의사 등록요청 */}
-      {selectedType === 'request' && (
+      {selectedType === "request" && (
         <List
           headers={currentHeaders}
           items={items}
-          listbutton={'상세보기'}
-          buttonType={'N'}
+          listbutton={"상세보기"}
+          buttonType={"N"}
         />
       )}
 
       {/*병원관리,약국관리 */}
-      {(selectedType === 'hospital' || selectedType === 'drugstore') && (
+      {(selectedType === "hospital" || selectedType === "drugstore") && (
         <List
           headers={currentHeaders}
           items={items}
-          buttonType={'Y'}
-          buttonName={'추가'}
-          listbutton={'수정'}
+          buttonType={"Y"}
+          buttonName={"추가"}
+          listbutton={"수정"}
         />
       )}
 
       {/*문의관리 */}
-      {(selectedType === 'userInquiry' ||
-        selectedType === 'doctorInquiry' ||
-        selectedType === 'drugstoreInquiry') && (
-        <List headers={currentHeaders} items={items} type={'Date'} />
+      {(selectedType === "userInquiry" ||
+        selectedType === "doctorInquiry" ||
+        selectedType === "drugstoreInquiry") && (
+        <List headers={currentHeaders} items={items} type={"Date"} />
       )}
     </div>
   );
--- src/Components/ModalComponent/Admin/MemberProfileEditModal.jsx
+++ src/Components/ModalComponent/Admin/MemberProfileEditModal.jsx
@@ -1,101 +1,155 @@
 // 회원정보수정 모달
-import React, { useState } from 'react';
-import Input from '../../Layout/Input';
-import Address from '../../Layout/Address';
+import React, { useState } from "react";
+import Input from "../../Layout/Input";
+import Address from "../../Layout/Address";
+import { useTokenContext } from "../../Context/TokenContext";
+import axios from "axios";
 
 const MemberProfileEdit = ({ onClose, item = {} }) => {
-  const [selectedName, setSelectedName] = useState('');
-  const [address, setAddress] = useState({ main: '', detail: '' });
+  const { token, userEmail } = useTokenContext();
+  // const [selectedName, setSelectedName] = useState("");
+  const [address, setAddress] = useState({
+    main: "",
+    detail: "",
+    subdetail: "",
+  });
+  const [memberId, setMemberId] = useState("");
+  const [memberBirth, setMemberBirth] = useState(item.memberBirth);
+  const [memberName, setMemberName] = useState(item.memberName);
+  const [memberEmail, setMemberEmail] = useState(item.memberEmail);
+  const [memberTel, setMemberTel] = useState(item.memberTel);
+  const [memberPwd, setMemberPwd] = useState("");
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      memberEmail: userEmail,
+    },
+  };
 
   const handleAddressSelect = (selectedAddress) => {
     setAddress(selectedAddress);
   };
 
+  const data = {
+    memberName: memberName,
+    memberEmail: memberEmail,
+    memberPwd: memberPwd,
+    memberTel: memberTel,
+    memberAddrMain: address.main,
+    memberAddrDetail: address.detail,
+    memberAddrSubdetail: address.subdetail,
+  };
+
+  const updateMemberInfo = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/members/updateMemberInfo",
+        data,
+        config
+      );
+
+      console.log("수정될 정보 : ", data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div
       style={{
-        width: '600px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        width: "600px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <table id='signUpInputForm' className='signUpTable'>
+      <table id="signUpInputForm" className="signUpTable">
         <tr>
           <td colSpan={2}>
             <Input
-              id='name'
-              className='member_name'
-              label='이름'
-              type='text'
-              placeholder='이름을 입력하세요'
-              style={{ width: '500px' }}
-              value={item.memberName}
-              onChange={(e) => setSelectedName(e.target.value)}
+              id="name"
+              className="member_name"
+              label="이름"
+              type="text"
+              placeholder="이름을 입력하세요"
+              style={{ width: "500px" }}
+              value={memberName}
+              onChange={(e) => setMemberName(e.target.value)}
             />
           </td>
         </tr>
         <tr>
           <td colSpan={2}>
             <Input
-              id='email'
-              className='member_email'
-              label='이메일'
-              type='text'
-              placeholder='　이메일형식'
-              style={{ width: '500px' }}
-              value={item.memberEmail}
+              id="email"
+              className="member_email"
+              label="이메일"
+              type="text"
+              placeholder="　이메일형식"
+              style={{ width: "500px" }}
+              value={memberEmail}
+              onChange={(e) => setMemberEmail(e.target.value)}
             />
           </td>
         </tr>
         <tr>
           <td colSpan={2}>
             <Input
-              id='pwd'
-              className='member_pwd'
-              label='비밀번호'
-              type='password'
-              style={{ width: '500px' }}
-              placeholder='　영어,숫자,특수문자를 포함한 8~20자 '
+              id="pwd"
+              className="member_pwd"
+              label="비밀번호"
+              type="password"
+              style={{ width: "500px" }}
+              placeholder="　영어,숫자,특수문자를 포함한 8~20자 "
               minLength={8}
               maxLength={20}
+              onChange={(e) => setMemberPwd(e.target.value)}
             />
           </td>
         </tr>
         <tr>
           <td colSpan={2}>
             <Input
-              id='tel'
-              className='member_tel'
-              label='전화번호'
-              type='text'
-              style={{ width: '500px' }}
+              id="tel"
+              className="member_tel"
+              label="전화번호"
+              type="text"
+              style={{ width: "500px" }}
               placeholder="　'-' 없이 입력하세요"
-              value={item.memberTel}
+              value={memberTel}
+              onChange={(e) => setMemberTel(e.target.value)}
             />
           </td>
         </tr>
         <tr>
-          <td colSpan={2} style={{ width: '510px' }}>
+          <td colSpan={2} style={{ width: "510px" }}>
             <Address
               onAddressSelect={handleAddressSelect}
               itemAddr={item.memberAddrMain}
               itemAddrDetail={item.memberAddrDetail}
+              itemAddrSubdetail={item.memberAddrSubdetail}
             />
           </td>
         </tr>
       </table>
-      <div className='modify-button'>
+      <div className="modify-button">
         <button
-          className='clinicSubBtn-short'
-          style={{ background: 'red', height: '50px' }}
+          className="clinicSubBtn-short"
+          style={{ background: "red", height: "50px" }}
         >
           제한
         </button>
-        <button className='clinicSubBtn-mid' style={{ background: '#11C2AD' }}>
+        <button
+          className="clinicSubBtn-mid"
+          onClick={updateMemberInfo}
+          style={{ background: "#11C2AD" }}
+        >
           저장
         </button>
-        <button className='clinicSubBtn-mid' onClick={onClose}>
+        <button className="clinicSubBtn-mid" onClick={onClose}>
           취소
         </button>
       </div>
