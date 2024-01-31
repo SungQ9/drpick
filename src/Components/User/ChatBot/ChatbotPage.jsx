a2a92029cdc92398a647df8c68ea09f0450f2ef8
pbt91 <parkbyungtae91@naver.com>
2024-01-31 17:14:21 +0900

    240131-#4

--- src/Components/User/ChatBot/ChatbotPage.jsx
+++ src/Components/User/ChatBot/ChatbotPage.jsx
@@ -1,4 +1,3 @@
-// React 및 필요한 라이브러리와 CSS 가져오기
 import React, { useEffect, useState } from "react";
 import $ from "jquery";
 import axios from "axios";
@@ -7,42 +6,39 @@ import {
   handleTags,
   handleDefaultResponse,
   site,
-  department,
+  department
 } from "./ChatbotResponse";
 
-// 사용자 이름을 저장할 변수 선언
-let userName = null;
+// 변수 (Variables)
+let userName = null; // 사용자 이름을 저장하는 변수
 
-// 챗봇 메시지 객체 정의
+// 챗봇 메시지를 만드는 함수
 function Message(arg) {
-  // 메시지의 내용, 위치(왼쪽 혹은 오른쪽), 링크, 링크 텍스트, 상태 저장
-  this.text = arg.text;
-  this.message_side = arg.message_side;
-  this.link = arg.link;
-  this.linkText = arg.linkText;
-  this.state = arg.state;
+  this.text = arg.text; // 메시지 텍스트
+  this.message_side = arg.message_side; // 메시지가 나타날 측면 (왼쪽 또는 오른쪽)
+  this.link = arg.link; // 링크 URL
+  this.linkText = arg.linkText; // 링크 텍스트
+  this.state = arg.state; // 상태 객체 (쿼리 파라미터로 전달될 수 있음)
 
   // 메시지를 화면에 그리는 함수
   this.draw = function () {
     let $message = $($(".chatbot_message_template").clone().html());
-
+  
     $message.addClass(this.message_side);
     $message.find(".chatbot_text").html(this.text);
-
+  
+    // 링크가 있으면 링크 추가
     if (this.link) {
       let href = this.link;
+      // 상태 객체를 쿼리 파라미터로 추가 (있는 경우)
       if (this.state) {
-        // 상태 객체를 URL 쿼리 파라미터로 변환
         const queryParams = new URLSearchParams(this.state).toString();
         href += `?${queryParams}`;
       }
-      const $link = $("<a>")
-        .attr("href", href)
-        .attr("target", "_blank")
-        .text(this.linkText);
+      const $link = $('<a>').attr('href', href).attr('target', '_blank').text(this.linkText);
       $message.find(".chatbot_text").append($link);
     }
-
+  
     $(".chatbot_messages").append($message);
     setTimeout(function () {
       $message.addClass("appeared");
@@ -52,15 +48,15 @@ function Message(arg) {
   return this;
 }
 
-// 입력된 메시지 텍스트를 가져오는 함수
+// 입력된 메시지를 가져오는 함수
 function getMessageText() {
-  let $message_input = $(".chatbot_message_input");
-  return $message_input.val();
+  let $message_input = $(".chatbot_message_input"); // 입력창 선택
+  return $message_input.val(); // 입력된 텍스트 반환
 }
 
-// 메시지 전송 함수
+// 메시지 전송 함수 (링크 텍스트 조건부 포함)
 function sendMessage(text, message_side, link, linkText, state) {
-  $(".chatbot_message_input").val("");
+  $(".chatbot_message_input").val(""); // 입력 필드 초기화
 
   let message = new Message({
     text: text + ": ",
@@ -70,17 +66,17 @@ function sendMessage(text, message_side, link, linkText, state) {
     state: state,
   });
 
-  message.draw();
+  message.draw(); // 메시지 창에 표시
   $(".chatbot_messages").animate(
     { scrollTop: $(".chatbot_messages").prop("scrollHeight") },
     300
-  );
+  ); // 스크롤 아래로 이동
 }
 
-// Enter 키 이벤트 처리 함수
+// Enter 키 입력 처리 함수
 function onClickAsEnter(e) {
   if (e && e.keyCode === 13) {
-    onSendButtonClicked();
+    onSendButtonClicked(); // 메시지 보내기 버튼 클릭과 동일한 행동
   }
 }
 
@@ -93,10 +89,10 @@ function setUserName(username) {
       null,
       null,
       null
-    );
+    ); // 사용자에게 환영 메시지 전송
     setTimeout(function () {
       sendMessage("무엇을 도와드릴까요", "left", null, null, null);
-    }, 2000);
+    }, 2000); // 추가 메시지 전송
     return username;
   } else {
     sendMessage("올바른 닉네임을 이용해주세요.", "left", null, null, null);
@@ -106,8 +102,9 @@ function setUserName(username) {
 
 // 사용자 환영 함수
 function greet() {
-  userName = localStorage.getItem("userName");
+  userName = localStorage.getItem("userName"); // 로컬 스토리지에서 사용자 이름 가져오기
   if (userName === null) {
+    // 사용자 이름이 없는 경우
     setTimeout(function () {
       return sendMessage(
         "로그인을 하지 않으셨습니다. 사용할 닉네임을 알려주세요.",
@@ -116,10 +113,11 @@ function greet() {
         null,
         null
       );
-    }, 2000);
+    }, 2000); // 일정 시간 후에 닉네임 요청 메시지 전송
   } else {
+    // 사용자 이름이 이미 저장되어 있는 경우
     setTimeout(function () {
-      sendMessage("반갑습니다, " + userName + "님.", "left", null, null, null);
+      sendMessage("반갑습니다, " + userName + "님.", "left", null, null, null); // 사용자에게 환영 메시지 전송
       setTimeout(function () {
         sendMessage(
           "안녕하세요,저는 Dr.Pick챗봇입니다.",
@@ -128,15 +126,15 @@ function greet() {
           null,
           null
         );
-      }, 1000);
+      }, 1000); // 조금 후에 추가 메시지 전송
       setTimeout(function () {
-        sendMessage("무엇을 도와드릴까요", "left", null, null, null);
-      }, 2000);
+        sendMessage("무었을 도와드릴까요", "left", null, null, null);
+      }, 2000); // 조금 후에 추가 메시지 전송
     }, 1000);
   }
 }
 
-// 채팅 요청을 서버에 보내는 함수
+// 채팅 요청을 보내는 함수
 function requestChat(messageText, url_pattern, state) {
   console.log(
     "AJAX요청 전송: " +
@@ -163,12 +161,44 @@ function requestChat(messageText, url_pattern, state) {
 
       console.log("input:", input, "entity:", entity);
 
-      // 엔티티 타입에 따라 적절한 메시지 전송
+      // 사이트 기능 문의시
       if (entity.includes("-SITE")) {
-        // 여기에 로직 구현
+        const {
+          text: siteText,
+          site: siteSite,
+          linkText: siteLinkText,
+        } = handleTags(input, "site");
+        console.log(
+          "text: ",
+          siteText,
+          "Site:",
+          siteSite,
+          "linkText:",
+          siteLinkText
+        );
+        sendMessage(siteText, "left", siteSite, siteLinkText, state);
+        foundEntity = true;
       }
-      // 다른 엔티티 타입에 대한 로직 추가
-      // ...
+      // 진료과 입력시
+      else if (entity.includes("-DEPARTMENT")) {
+        const {
+          text: deptText,
+          site: deptSite,
+          linkText: deptLinkText,
+        } = handleTags(input, "department");
+        sendMessage(deptText, "left", deptSite, deptLinkText, state);
+        foundEntity = true;
+      }
+      // 증상 입력시
+      else if (entity.includes("-SYMPTOM")) {
+        const {
+          text: symText,
+          site: symSite,
+          linkText: symLinkText,
+        } = handleTags(input, "symptoms");
+        sendMessage(symText, "left", symSite, symLinkText, state);
+        foundEntity = true;
+      }
 
       if (!foundEntity) {
         sendMessage(handleDefaultResponse(), "left", null, null, state);
@@ -182,28 +212,53 @@ function requestChat(messageText, url_pattern, state) {
         null,
         null,
         state
-      );
+      ); // 오류 발생 시 메시지 전송
     });
 }
 
-// 메시지 보내기 버튼 클릭 처리 함수
+// 메시지 보내기 버튼을 클릭할 때 처리하는 함수
 function onSendButtonClicked() {
-  let messageText = getMessageText();
-  sendMessage(messageText, "right", null, null, null);
+  let messageText = getMessageText(); // 입력된 메시지 텍스트 가져오기
+  sendMessage(messageText, "right", null, null, null); // 입력된 메시지를 화면에 표시 (사용자가 보낸 메시지)
 
-  // 사용자 이름 설정 및 대화 로직
   if (userName == null) {
-    // 여기에 로직 구현
+    userName = setUserName(messageText); // 사용자 이름이 없으면 설정
   } else {
-    // 여기에 로직 구현
+    if (messageText.includes("안녕")) {
+      setTimeout(function () {
+        return sendMessage(
+          "안녕하세요. 저는 Dr.Pick챗봇입니다.",
+          "left",
+          null,
+          null,
+          null
+        );
+      }, 1000); // '안녕' 포함된 경우 환영 메시지 전송
+    } else if (messageText.includes("고마워")) {
+      setTimeout(function () {
+        return sendMessage(
+          "천만에요. 더 물어보실 건 없나요?",
+          "left",
+          null,
+          null,
+          null
+        );
+      }, 1000); // '고마워' 포함된 경우 감사 메시지 전송
+    } else if (messageText.includes("없어")) {
+      setTimeout(function () {
+        return sendMessage("그렇군요. 알겠습니다!", "left", null, null, null);
+      }, 1000); // '없어' 포함된 경우 확인 메시지 전송
+    } else {
+      return requestChat(messageText, "request_chat", null); // 일반 대화 요청
+    }
   }
 }
 
 // ChatbotPage 컴포넌트 정의
 function ChatbotPage() {
   useEffect(() => {
-    greet();
-    onClickAsEnter();
+    greet(); // 페이지가 로드될 때 사용자 환영 함수 실행
+    onClickAsEnter(); // Enter 키 입력 처리 함수 실행
   }, []);
 
   return (
